2ed3cdc5440f4bcf01ff35686e3a372e38a487fd
Hoang Van Nguyen <hoang.nguyenvan@asnet.com.vn>
2024-07-31 10:12:54 +0700

    Update code

--- src/libs/pages/LandingPage/index.tsx
+++ src/libs/pages/LandingPage/index.tsx
@@ -25,18 +25,31 @@ const LandingPageBase = () => {
         <Typography>Oops! An error occurred in Landing page.</Typography>
       }
     >
-      <Stack justifyContent="center" alignItems="center" height="90vh">
+      <Stack
+        position="absolute"
+        top="50%"
+        left="50%"
+        alignItems="center"
+        width={{ xs: "100%", lg: "500px" }}
+        sx={{
+          transform: "translate(-50%, -50%)",
+        }}
+      >
         <Typography
           variant="h1"
-          lineHeight="70px"
+          maxWidth="400px"
+          lineHeight="65px"
+          width="365px"
+          textAlign="center"
           textTransform="uppercase"
-          fontSize={{ xs: "60px", sm: "75px" }}
+          fontSize={{ xs: "50px", sm: "64px" }}
         >
           Welcome!
         </Typography>
         <Typography
           fontSize="12px"
           mb="16px"
+          maxWidth="340px"
           letterSpacing={{ xs: "2px", sm: "4px" }}
         >
           This user management application
@@ -46,7 +59,7 @@ const LandingPageBase = () => {
           flexItem
           sx={{ width: "30%", margin: "0 auto" }}
         />
-        <Stack flexDirection="row" mt="20px" gap="16px">
+        <Stack flexDirection="row" mt="20px" maxWidth="170px" gap="16px">
           <Button
             onClick={handleReturnLogin}
             variant="outlined"
